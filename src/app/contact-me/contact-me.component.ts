61fee07de42ad08c07e892439d4e231c09de4cfd
David Om <david.om582@gmail.com>
2019-02-10 21:08:57 -0500

    removed routerlink, everything is all on one page now

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-me.component.css']
 })
 export class ContactMeComponent implements OnInit {
+  contactForm:  FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.contactForm = new FormGroup({
+      'name': new FormControl(null),
+      'email': new FormControl(null)
+    });
   }
 
+  onSubmit() {
+    console.log(this.contactForm);
+    this.contactForm.reset();
+  }
 }
